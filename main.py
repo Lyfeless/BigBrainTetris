1ea5159ff777a2104cb1cc965f16c0e66f3f7b85
Logan Steffen <55418381+KirbsSE@users.noreply.github.com>
2019-09-16 20:41:04 -0500

    color

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ window = pyglet.window.Window()
 text = pyglet.text.Label("Suck my phat chode",
                          font_name = "Comic Sans MS",
                          font_size = 36,
+                         color = (0,0,255,255),
                          x = window.width / 2, y = window.height / 2,
                          anchor_x = "center", anchor_y = "center")
 
