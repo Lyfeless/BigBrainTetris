6dbf20499d4c1afe00441b83e161a60714a38bd0
Alex W <44251942+Lyfeless@users.noreply.github.com>
2019-09-17 14:17:13 -0500

    Added Dynamic and Static Block Rendering

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+import math
+
 import pyglet
 from pyglet.window import key
 
@@ -24,7 +27,20 @@ TILE_SIZE = 30
 
 # Board
 
-board = [[0 for x in range(BOARD_WIDTH)] for x in range(BOARD_HEIGHT)]
+board = []
+movingBlocks = []
+
+# Images
+
+blockBatch = pyglet.graphics.Batch()
+moveBatch = pyglet.graphics.Batch()
+
+blockImg = pyglet.image.load('Assets/Tetromino Block.png')
+blockTexture = blockImg.get_texture()
+blockImg.width = TILE_SIZE
+blockImg.height = TILE_SIZE
+blockTexture.width = TILE_SIZE
+blockTexture.height = TILE_SIZE
 
 # Input
 
@@ -61,6 +77,56 @@ def update(dt):
     if inputs[key.LEFT]:
         text_x -= 1
 
+    for moveBlock in movingBlocks:
+        moveBlock.update()
+
+"""
+
+    CLASSES
+
+"""
+
+class MoveBlock:
+    def __init__(self, x, y, target_x, target_y, scale, image):
+        global board
+        
+        self.x        =  x
+        self.y        =  y
+        self.target_x =  target_x
+        self.target_y =  target_y
+        self.scale    =  scale
+        self.sprite   =  pyglet.sprite.Sprite(image, self.x, self.y, batch = moveBatch)
+
+        board[math.ceil(y / TILE_SIZE)][math.ceil(x / TILE_SIZE)].visible = 0
+
+    def update(self):
+        global board
+        global movingBlocks
+
+        print(str(self.x) + ", " + str(self.y))
+
+        distance_x = (self.target_x - self.x)
+        distance_y = (self.target_y - self.y)
+
+        self.x += distance_x / self.scale
+        self.y += distance_y / self.scale
+
+        self.sprite.x = self.x
+        self.sprite.y = self.y
+
+        if distance_x < 1 and distance_y < 1:
+            
+            tile_x = math.ceil(self.x / TILE_SIZE)
+            tile_y = math.ceil(self.y / TILE_SIZE)
+            
+            if (tile_x >= 0 and tile_x < len(board[0])) and (tile_y >= 0 and tile_y < len(board)):
+                board[tile_y][tile_x].visible = 1
+
+            print("Tile: " + str(tile_x) + ", " + str(tile_y))
+
+                
+            movingBlocks.remove(self)
+            
 
 """
 
@@ -72,16 +138,17 @@ def update(dt):
 def on_draw():
     window.clear()
     text.draw();
+    blockBatch.draw()
+    moveBatch.draw()
+    #blockImg.blit(0,0)
 
 @window.event
 def on_key_press(symbol, modifiers):
     inputs[symbol] = True
-    print("pressed " + str(symbol))
     
 @window.event
 def on_key_release(symbol, modifiers):
     inputs[symbol] = False
-    print("depressed " + str(symbol))
 
 """
 
@@ -90,6 +157,22 @@ def on_key_release(symbol, modifiers):
 """
 
 window.set_size(BOARD_WIDTH * TILE_SIZE, BOARD_HEIGHT * TILE_SIZE)
+
+board  = [[pyglet.sprite.Sprite(blockImg, x * TILE_SIZE, y * TILE_SIZE, batch=blockBatch) for x in range(BOARD_WIDTH)] for y in range(BOARD_HEIGHT)]
+
+
+for y in range(len(board)):
+    for x in range(len(board[y])):
+        if random.randint(0, 1) == 1:
+            board[y][x].visible = False
+        else:
+            board[y][x].visible = False
+
+board[0][0].visible = True
+board[1][0].visible = True
+
+
+movingBlocks.append(MoveBlock(0, TILE_SIZE, TILE_SIZE * 5, TILE_SIZE * 7, 12, blockImg))
 
 pyglet.clock.schedule_interval(update,1/120.0)
 pyglet.app.run() # Run
